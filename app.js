f1a65bd431440f04570e0ed9653ddf6ef21ba81b
Florian Brandel <mail@fbrandel.de>
2015-03-14 16:18:04 +0100

    Added process.env output

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!!');
+app.get('/', function(req, res) {
+
+	var output = '<h1>Environment Variables</h1>';
+	output += '<pre>';
+	for (var env in process.env) {
+		output += env + ': ' + process.env[env] + require('os').EOL;
+	}
+	output += '</pre>';
+	res.send(output);
 })
 
-var server = app.listen(app.get('port'), function () {
+var server = app.listen(app.get('port'), function() {
 
-  var host = server.address().address
-  var port = server.address().port
+	var host = server.address().address
+	var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
-
+	console.log('Example app listening at http://%s:%s', host, port)
 })
